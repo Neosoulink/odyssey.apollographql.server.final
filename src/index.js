a370dcd2931c855796c53ecfb5e48d939e2134be
Neosoulink <neodarksoulink@gmail.com>
2022-02-18 10:14:33 +0100

    use port from env variable

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./schema');
-const resolvers = require('./resolvers');
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
 
-const TrackAPI = require('./datasources/track-api');
+const TrackAPI = require("./datasources/track-api");
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  dataSources: () => {
-    return {
-      trackAPI: new TrackAPI(),
-    };
-  },
+	typeDefs,
+	resolvers,
+	dataSources: () => {
+		return {
+			trackAPI: new TrackAPI(),
+		};
+	},
 });
 
-server.listen().then(() => {
-  console.log(`
+server.listen({ port: process.env.PORT || 4000 }).then(() => {
+	console.log(`
     🚀  Server is running!
     🔉  Listening on port 4000
     📭  Query at https://studio.apollographql.com/dev
